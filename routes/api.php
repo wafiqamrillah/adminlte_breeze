f1fd22791ed78337251c10ad0d62d2c346f5f49b
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2023-11-23 11:45:54 +0700

    ci(api): Try to implement API into Laravel

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,11 +15,30 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::name('api.')->group(function() {
+    Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+        return $request->user();
+    });
+    
+    Route::middleware('guest')->group(function() {
+        Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('login');
+        Route::post('/register', [RegisteredUserController::class, 'store'])->name('register');
+        Route::name('password')->group(function() {
+            Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])->name('.email');
+            Route::post('/reset-password', [NewPasswordController::class, 'store'])->name('.store');
+        });
+    });
+    
+    Route::middleware(['auth'])->group(function() {
+        Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+        Route::middleware(['throttle:6,1'])->name('verification')->group(function() {
+            Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)->middleware(['signed'])->name('.verify');
+            Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])->name('.send');
+        });
+    });
 });
