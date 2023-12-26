35e40dd958ec4466d1b22974b1dd8ed6b99f9dbf
Ahmad Wafiq Amrillah <wafiqamrillah@gmail.com>
2023-12-26 09:24:58 +0700

    build(datatable): Import Livewire Datatable JS into app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,7 @@
 import './modal';
+import '../../vendor/rappasoft/laravel-livewire-tables/resources/imports/laravel-livewire-tables';
+import '../../vendor/rappasoft/laravel-livewire-tables/resources/imports/laravel-livewire-tables-all';
+
 import './bootstrap';
 
 import.meta.glob([
